c5cce04a6a9ea7447c6cf20348a74ac9375af560
naoufel_raouine <naoufel.raouine@gmail.com>
2019-06-29 15:02:37 +0200

    api product list

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -4,6 +4,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Produit;
+
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+ 
+
 
 class ProduitController extends AbstractController
 {
@@ -12,8 +22,21 @@ class ProduitController extends AbstractController
      */
     public function index()
     {
-        return $this->render('produit/index.html.twig', [
-            'controller_name' => 'ProduitController',
+        $em=$this->getdoctrine();
+        $repoProduits=$em->getRepository(Produit::class);
+        $produits= $repoProduits->findAll();
+        //var_dump($produits);
+        $encoders = new JsonEncoder();
+        $normalizers = new ObjectNormalizer();
+        $serializer = new Serializer([$normalizers], [$encoders]);
+        $jsonContent = $serializer->serialize($produits, 'json',[
+            'circular_reference_handler' => function ($object) {
+            return $object->getId();
+            }
         ]);
+        //var_dump($jsonContent);exit;
+        $response = new Response($jsonContent);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response; 
     }
 }
